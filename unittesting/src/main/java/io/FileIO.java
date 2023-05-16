50f4c86621c1be6b0498500b3b4bdd0c3bbe3078
gzark1 <t8200043@aueb.gr>
2023-05-16 14:35:08 +0300

    Delete src from .gitignore file

---
+++ unittesting/src/main/java/io/FileIO.java
@@ -0,0 +1,55 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+* The FileIO provides simple file input/output operations
+* that serve as hands-on practice on Unit Testing.
+*
+* @author  agkortzis
+* @version 1.0
+* @since   2020-04-06 
+*/
+public class FileIO {
+	
+	/**
+	 * Reads a file that contains numbers line by line 
+	 * and returns an array of the integers found in the file.
+	 * @param filepath the file that contains the numbers
+	 * @return an array of numbers
+	 * @exception IllegalArgumentException when the given file does not exist
+	 * @exception IllegalArgumentException when the given file is empty
+	 * @exception IOException when an IO interruption occurs (not required to be tested)
+	 */
+	public int[] readFile(String filepath) {
+		File file = new File(filepath);
+		if (!file.exists()) 
+			throw new IllegalArgumentException("Input file does not exist");
+		
+		List<Integer> numbersList = new ArrayList<>();
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				int number = Integer.parseInt(line);
+				numbersList.add(number);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		if (numbersList.size() == 0) 
+			throw new IllegalArgumentException("Given file is empty");
+		
+		// Convert a List to an array using 
+		return numbersList.stream().mapToInt(i -> i).toArray();
+	}
+
+}
---
+++ unittesting/src/test/java/math/ArithmeticOperationsTest.java
@@ -0,0 +1,13 @@
+package math; //same package structure with the functional class whose code it is testing.
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArithmeticOperationsTest {
+
+    @Test
+    void test(){
+
+    }
+}
