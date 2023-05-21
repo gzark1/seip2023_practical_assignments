dd2c7d0cbc683bb86ffbff5d81d17b2404e0a4fa
gzark1 <t8200043@aueb.gr>
2023-05-22 00:25:03 +0300

    Create isPrime method and all necessary test cases

--- unittesting/src/main/java/math/MyMath.java
+++ unittesting/src/main/java/math/MyMath.java
@@ -1,7 +1,7 @@
 package math;
 
 public class MyMath {
-    public int factorial (int n){
+    public int factorial (int n) {
         if (n < 0 || n > 12) {
             throw new IllegalArgumentException();
         }
@@ -10,5 +10,23 @@ public class MyMath {
             int result = n * factorial(n-1);
             return result;
         }
+    }
+
+    public boolean isPrime(int n) {
+        // Check if n is less than 2, as prime numbers start from 2
+        if (n < 2) {
+            throw new IllegalArgumentException("number should be >= 2");
+        }
+
+        // Check for divisibility by numbers from 2 to the square root of n
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                // If n is divisible by any number, it is not prime
+                return false;
+            }
+        }
+
+        // If no divisors are found, n is prime
+        return true;
     }
 }
--- unittesting/src/test/java/math/MyMathTest.java
+++ unittesting/src/test/java/math/MyMathTest.java
@@ -1,6 +1,9 @@
 package math;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import static org.junit.Assert.*;
 
 public class MyMathTest{
@@ -14,5 +17,43 @@ public class MyMathTest{
     public void factorialOf13ShouldThrowIllegalArgumentException() {
         MyMath myMathObj = new MyMath();
         int result = myMathObj.factorial(13);
+    }
+
+    @Test
+    public void testIsPrimeWithPrimeNumbers() {
+        MyMath myMathObj = new MyMath();
+
+        assertTrue(myMathObj.isPrime(2));
+        assertTrue(myMathObj.isPrime(3));
+        assertTrue(myMathObj.isPrime(5));
+        assertTrue(myMathObj.isPrime(7));
+        assertTrue(myMathObj.isPrime(36787));
+        assertTrue(myMathObj.isPrime(4261));
+        assertTrue(myMathObj.isPrime(6977));
+    }
+
+    @Test
+    public void testIsPrimeWithNonPrimeNumbers() {
+        MyMath myMathObj = new MyMath();
+
+        assertFalse(myMathObj.isPrime(999));
+        assertFalse(myMathObj.isPrime(4));
+        assertFalse(myMathObj.isPrime(6));
+        assertFalse(myMathObj.isPrime(8));
+        assertFalse(myMathObj.isPrime(10002));
+        assertFalse(myMathObj.isPrime(9372));
+        assertFalse(myMathObj.isPrime(111));
+    }
+
+    @Rule
+    public final ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void isPrimeShouldThrowIllegalArgumentExceptionIfNumberLessthan2() {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("number should be >= 2");
+
+        MyMath myMathObj = new MyMath();
+        boolean flag = myMathObj.isPrime(1);
     }
 }
